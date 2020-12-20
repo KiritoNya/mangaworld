d5e96acb6e7cdd1658941627bab2bc1a80739b3a
KiritoNya <watashiwayuridaisuki@gmail.com>
2020-12-20 21:24:34 +0100

    Add method "SearchState"

--- state.go
+++ state.go
@@ -9,3 +9,19 @@ const (
 	Paused    State = "In pausa"
 	Releasing State = "In corso"
 )
+
+func searchState(stat State) string {
+	switch stat {
+	case Deleted:
+		return "canceled"
+	case Dropped:
+		return "dropped"
+	case Finish:
+		return "completed"
+	case Paused:
+		return "paused"
+	case Releasing:
+		return "ongoing"
+	}
+	return ""
+}
