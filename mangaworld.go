f50fe2c992bb866adc78dcd1c671cdcc2b0d9d6c
KiritoNya <watashiwayuridaisuki@gmail.com>
2020-12-20 21:25:05 +0100

    Add functions search

--- mangaworld.go
+++ mangaworld.go
@@ -1,11 +1,5 @@
 package mangaworld
 
-import (
-	"github.com/KiritoNya/htmlutils"
-	"golang.org/x/net/html"
-	"net/http"
-)
-
 /*type Mangaworld struct {
 	Url string
 }*/
@@ -13,42 +7,26 @@ import (
 const UrlSite = "https://www.mangaworld.cc/"
 const UrlSearch = "https://www.mangaworld.cc/archive?"
 
-/*func New(url string) *Mangaworld {
-	return &Mangaworld{Url: url}
-}*/
+func SearchByName(name string) (manga []Manga, err error) {
+	q := NewQuery()
+	q.SetMangaName(name)
+	return q.Do()
+}
 
-func (q *Query) Search(url string) (mangas []Manga, err error) {
+func SearchByGenre(genres []Genre) (manga []Manga, err error) {
+	q := NewQuery()
+	q.SetGenres(genres)
+	return q.Do()
+}
 
-	url = q.createQuery()
+func SearchByType(types []Type) (manga []Manga, err error) {
+	q := NewQuery()
+	q.SetMangaTypes(types)
+	return q.Do()
+}
 
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	htmlNode, err := html.Parse(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	divs, err := htmlutils.QuerySelector(htmlNode, "div", "class", "entry")
-	if err != nil {
-		return nil, err
-	}
-
-	for _, div := range divs {
-		tagA, err := htmlutils.GetGeneralTags(div, "a")
-		if err != nil {
-			return nil, err
-		}
-
-		url, err := htmlutils.GetValueAttr(tagA[0], "a", "href")
-		if err != nil {
-			return nil, err
-		}
-
-		mangas = append(mangas, Manga{Url: string(url[0])})
-	}
-	return mangas, nil
+func SearchByStatus(states []State) (manga []Manga, err error) {
+	q := NewQuery()
+	q.SetStatus(states)
+	return q.Do()
 }
--- mangaworld_test.go
+++ mangaworld_test.go
@@ -2,8 +2,29 @@ package mangaworld
 
 import "testing"
 
-func TestSearchByName(t *testing.T) {
+func TestSearchByGenre(t *testing.T) {
+	var result = []Manga{
+		{Url: "https://www.mangaworld.cc/manga/734/rain"},
+		{Url: "https://www.mangaworld.cc/manga/817/6-hours-and-5-minutes"},
+	}
 
+	mangas, err := SearchByGenre([]Genre{Yuri})
+	if err != nil {
+		t.Fatal("Error to search manga: ", err)
+	}
+
+	mangas = mangas[:2]
+
+	for i, manga := range mangas {
+		if manga.Url != result[i].Url {
+			t.Error("Error not obtain", result[i].Url, "but obtain", manga.Url)
+		} else {
+			t.Log("Search manga", i, "[OK]")
+		}
+	}
+}
+
+func TestSearchByName(t *testing.T) {
 	var result = []Manga{
 		{Url: "https://www.mangaworld.cc/manga/395/citrus"},
 		{Url: "https://www.mangaworld.cc/manga/1876/citrus-1"},
@@ -18,19 +39,18 @@ func TestSearchByName(t *testing.T) {
 		if manga.Url != result[i].Url {
 			t.Error("Error not obtain", result[i].Url, "but obtain", manga.Url)
 		} else {
-			t.Log("Search manga [OK]")
+			t.Log("Search manga", i, "[OK]")
 		}
 	}
 }
 
-func TestSearchByGenre(t *testing.T) {
-
+func TestSearchByType(t *testing.T) {
 	var result = []Manga{
-		{Url: "https://www.mangaworld.cc/manga/734/rain"},
-		{Url: "https://www.mangaworld.cc/manga/817/6-hours-and-5-minutes"},
+		{Url: "https://www.mangaworld.cc/manga/426/1-2-cigarette"},
+		{Url: "https://www.mangaworld.cc/manga/1151/1-3-romantica"},
 	}
 
-	mangas, err := SearchByGenre("citrus")
+	mangas, err := SearchByType([]Type{Manga_type})
 	if err != nil {
 		t.Fatal("Error to search manga: ", err)
 	}
@@ -41,7 +61,29 @@ func TestSearchByGenre(t *testing.T) {
 		if manga.Url != result[i].Url {
 			t.Error("Error not obtain", result[i].Url, "but obtain", manga.Url)
 		} else {
-			t.Log("Search manga [OK]")
+			t.Log("Search manga", i, "[OK]")
+		}
+	}
+}
+
+func TestSearchByStatus(t *testing.T) {
+	var result = []Manga{
+		{Url: "https://www.mangaworld.cc/manga/1983/1-3-sanbun-no-ichi"},
+		{Url: "https://www.mangaworld.cc/manga/1895/100-nichigo-ni-kuufuku-de-taore-maid-ni-naru-onna-no-ko"},
+	}
+
+	mangas, err := SearchByStatus([]State{Releasing})
+	if err != nil {
+		t.Fatal("Error to search manga: ", err)
+	}
+
+	mangas = mangas[:2]
+
+	for i, manga := range mangas {
+		if manga.Url != result[i].Url {
+			t.Error("Error not obtain", result[i].Url, "but obtain", manga.Url)
+		} else {
+			t.Log("Search manga", i, "[OK]")
 		}
 	}
 }
