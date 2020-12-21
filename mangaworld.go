7cf3366cab0f6a60176c37dbfaf5face6928b6b6
Kirito <watashiwayuridaisuki@gmail.com>
2020-12-21 20:05:14 +0100

    Add other functions

--- mangaworld.go
+++ mangaworld.go
@@ -1,8 +1,11 @@
 package mangaworld
 
-/*type Mangaworld struct {
-	Url string
-}*/
+import (
+	"github.com/KiritoNya/htmlutils"
+	"golang.org/x/net/html"
+	"net/http"
+	"strconv"
+)
 
 const UrlSite = "https://www.mangaworld.cc/"
 const UrlSearch = "https://www.mangaworld.cc/archive?"
@@ -29,4 +32,132 @@ func SearchByStatus(states []State) (manga []Manga, err error) {
 	q := NewQuery()
 	q.SetStatus(states)
 	return q.Do()
+}
+
+func TrendingManga() (mangaTrend []Trending, err error) {
+	resp, err := http.Get(UrlSite)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	node, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	track, err := htmlutils.QuerySelector(node, "div", "id", "chapters-slide")
+	if err != nil {
+		return nil, err
+	}
+
+	divs, err := htmlutils.QuerySelector(track[1], "div", "class", "entry vertical")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, div := range divs {
+		tm, err := NewTrendingManga(div)
+		if err != nil {
+			return nil, err
+		}
+
+		err = tm.GetManga()
+		if err != nil {
+			return nil, err
+		}
+
+		err = tm.GetChapter()
+		if err != nil {
+			return nil, err
+		}
+
+		mangaTrend = append(mangaTrend, *tm)
+	}
+	return mangaTrend, err
+}
+
+func MonthlyManga() (mangas []Manga, err error) {
+	resp, err := http.Get(UrlSite)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	node, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	divs, err := htmlutils.QuerySelector(node, "div", "class", "top-wrapper")
+	if err != nil {
+		return nil, err
+	}
+
+	entries, err := htmlutils.QuerySelector(divs[1], "div", "class", "short")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		tagsA, err := htmlutils.GetGeneralTags(entry, "a")
+		if err != nil {
+			return nil, err
+		}
+
+		url, err := htmlutils.GetValueAttr(tagsA[0], "a", "href")
+		if err != nil {
+			return nil, err
+		}
+
+		mangas = append(mangas, Manga{Url: string(url[0])})
+	}
+	return mangas, nil
+}
+
+func ChaptersNew(num int) (chapters []ChapterNew, err error) {
+
+	for k := 0; k <= (num / 17); k++ {
+
+		resp, err := http.Get(UrlSite + "?page=" + strconv.Itoa(k+1))
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		node, err := html.Parse(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		grid, err := htmlutils.QuerySelector(node, "div", "class", "comics-grid")
+		if err != nil {
+			return nil, err
+		}
+
+		chDivs, err := htmlutils.QuerySelector(grid[0], "div", "class", "entry")
+		if err != nil {
+			return nil, err
+		}
+
+		for i := 0; i < num; i++ {
+			cn, err := NewChapterNew(chDivs[i])
+			if err != nil {
+				return nil, err
+			}
+
+			err = cn.GetChapter()
+			if err != nil {
+				return nil, err
+			}
+
+			err = cn.GetManga()
+			if err != nil {
+				return nil, err
+			}
+
+			chapters = append(chapters, *cn)
+		}
+	}
+	return nil, nil
 }
--- mangaworld_test.go
+++ mangaworld_test.go
@@ -1,6 +1,8 @@
 package mangaworld
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearchByGenre(t *testing.T) {
 	var result = []Manga{
@@ -84,6 +86,73 @@ func TestSearchByStatus(t *testing.T) {
 			t.Error("Error not obtain", result[i].Url, "but obtain", manga.Url)
 		} else {
 			t.Log("Search manga", i, "[OK]")
+		}
+	}
+}
+
+func TestMonthlyManga(t *testing.T) {
+
+	var result = []Manga{
+		{Url: "https://www.mangaworld.cc/manga/1972/martial-peak"},
+		{Url: "https://www.mangaworld.cc/manga/1929/tales-of-demons-and-gods"},
+		{Url: "https://www.mangaworld.cc/manga/1847/boku-no-hero-academia"},
+		{Url: "https://www.mangaworld.cc/manga/1738/solo-leveling"},
+		{Url: "https://www.mangaworld.cc/manga/1816/shingeki-no-kyojin"},
+		{Url: "https://www.mangaworld.cc/manga/1708/one-piece"},
+		{Url: "https://www.mangaworld.cc/manga/1674/jujutsu-kaisen"},
+		{Url: "https://www.mangaworld.cc/manga/716/kimetsu-no-yaiba"},
+		{Url: "https://www.mangaworld.cc/manga/2278/berserk"},
+		{Url: "https://www.mangaworld.cc/manga/1747/black-clover"},
+	}
+
+	mangas, err := MonthlyManga()
+	if err != nil {
+		t.Error("Error to ge monthly manga:", err)
+	}
+
+	for i, manga := range mangas {
+		if manga.Url != result[i].Url {
+			t.Error("Error not obtain", result[i].Url, "but obtain", manga.Url)
+		} else {
+			t.Log("Monthly manga", i, "[OK]")
+		}
+	}
+}
+
+func TestChaptersNew(t *testing.T) {
+	var result = []ChapterNew{
+		{
+			MangaNew: Manga{
+				Url: "https://www.mangaworld.cc/manga/1984/creepy-cat/", //It changes constantly
+			},
+			Chapters: []Chapter{
+				{Url: "https://www.mangaworld.cc/manga/1984/creepy-cat/read/5fe0df428d9b52437fffc98c"},
+				{Url: "https://www.mangaworld.cc/manga/1984/creepy-cat/read/5fdf37356fe15b440da486de"},
+				{Url: "https://www.mangaworld.cc/manga/1984/creepy-cat/read/5fde5f001ff3df43a02fe365"},
+			},
+		},
+		{
+			MangaNew: Manga{
+				Url: "https://www.mangaworld.cc/manga/1661/shuumatsu-no-walkuere/", //It changes constantly
+			},
+			Chapters: []Chapter{
+				{Url: "https://www.mangaworld.cc/manga/1661/shuumatsu-no-walkuere/read/5fe0dd5ff2ab2d434dcf488b/1"},
+			},
+		},
+	}
+
+	cn, err := ChaptersNew(2)
+	if err != nil {
+		t.Fatal("Error to get chapters new:", err)
+	}
+
+	for k, chapterNew := range cn {
+		for i, chapter := range chapterNew.Chapters {
+			if chapter.Url != result[k].Chapters[i].Url {
+				t.Error("Error not obtain", result[k].Chapters[i].Url, "but obtain", chapter.Url)
+			} else {
+				t.Log("Chapter new", i, "[OK]")
+			}
 		}
 	}
 }
